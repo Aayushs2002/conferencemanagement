8c051f5852503a19f45e450fd64e165b1d79d3b7
aayush <aayush@softsaro.com>
2025-10-26 12:48:45 +0545

    deploy test

--- resources/views/frontend/main-page/home/index.blade.php
+++ resources/views/frontend/main-page/home/index.blade.php
@@ -20,7 +20,7 @@
                         </a>
                     </div>
                 </div>
-
+ 
                 <div class="col-lg-6 hero-image">
                     <img src="{{ asset('frontend/assets/img/Group 9.png') }}" alt="Medical Conference" class="img-fluid">
                 </div>
