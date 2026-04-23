7c13a43453352c2871489051504a1a0ab11f76e7
Aayush Timilsina <timilsina.aayush@gmail.com>
2026-04-23 21:42:32 +0545

    certificate abbrebation name uncomment

--- resources/views/backend/conference/conference-registration/generate-certificate.blade.php
+++ resources/views/backend/conference/conference-registration/generate-certificate.blade.php
@@ -236,7 +236,7 @@ header('Access-Control-Allow-Origin: *');
 
                                 <td width="550" style="text-align:left; font-size:80px; font-weight:bold;color:red;">
 
-                                    {{-- {{ $conference->abbreviation ?? 'CONFERENCE' }} --}}
+                                    {{ $conference->abbreviation ?? 'CONFERENCE' }}
                                 </td>
 
                                 <td width="450" style="text-align:left;">&nbsp;
